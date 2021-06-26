f0fa6bb8ab697268cb1d7755376fb5639af1876b
Guruprasad Veerannavaru <guruprasadbv4648@gmail.com>
2021-06-26 23:31:49 +0530

    zero

--- src/config/index.js
+++ src/config/index.js
@@ -7,8 +7,8 @@ if (process.env.REACT_APP_ENV === 'production') {
     }
 } else {
     config = {
-        apiURL: "http://localhost:3080/api",
-        contractAddress: "0x8dA55c57653Fb16B05d2487Fd9279c483a2c25C9",
+        apiURL: "https://blockchain-election-backend.azurewebsites.net/api",
+        contractAddress: "0x017E083C6DaC9faff97523E3D94Be248a476c613",
     }
 }
 
