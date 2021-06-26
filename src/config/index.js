d4f93b81328a117eafc9fd681ebf99aebd2670be
Guruprasad Veerannavaru <guruprasadbv4648@gmail.com>
2021-06-26 23:19:49 +0530

    changed link

--- src/config/index.js
+++ src/config/index.js
@@ -2,8 +2,8 @@ let config
 
 if (process.env.REACT_APP_ENV === 'production') {
     config = {
-        apiURL: "http://localhost:3080/api",
-        contractAddress: "0x8dA55c57653Fb16B05d2487Fd9279c483a2c25C9",
+        apiURL: "https://blockchain-election-backend.azurewebsites.net/api",
+        contractAddress: "0x017E083C6DaC9faff97523E3D94Be248a476c613",
     }
 } else {
     config = {
