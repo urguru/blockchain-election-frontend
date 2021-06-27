70870d3ed7d2d346005d70f477f79c3a478db384
Guruprasad Veerannavaru <guruprasadbv4648@gmail.com>
2021-06-27 14:25:08 +0530

    new contract

--- src/config/index.js
+++ src/config/index.js
@@ -3,12 +3,12 @@ let config
 if (process.env.REACT_APP_ENV === 'production') {
     config = {
         apiURL: "https://blockchain-election-backend.azurewebsites.net/api",
-        contractAddress: "0x81D594065C0D5567d77523cd6D4E0B846B291Ec7",
+        contractAddress: "0x4183ad7951B29fd916272b330Ca4a5D33E7bba0a",
     }
 } else {
     config = {
         apiURL: "https://blockchain-election-backend.azurewebsites.net/api",
-        contractAddress: "0x81D594065C0D5567d77523cd6D4E0B846B291Ec7",
+        contractAddress: "0x4183ad7951B29fd916272b330Ca4a5D33E7bba0a",
     }
 }
 
