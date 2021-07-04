e67dd087c0dbfc4c9b9b0ef53ddc60e5543bfd50
7df47682a12c57601bf08fbaaba7e7417e0b9e33 <pruthvi.m@amagi.com>
2021-07-04 23:37:14 +0530

    restart

--- src/config/index.js
+++ src/config/index.js
@@ -3,12 +3,12 @@ let config
 if (process.env.REACT_APP_ENV === 'production') {
     config = {
         apiURL: "https://blockchain-election-backend.azurewebsites.net/api",
-        contractAddress: "0xc49998Eb3C5Df61E9b321F5eAeB7De4DC7e024ca",
+        contractAddress: "0x953513d3AD75027120493679eC6dD024bb98ee10",
     }
 } else {
     config = {
         apiURL: "https://blockchain-election-backend.azurewebsites.net/api",
-        contractAddress: "0xc49998Eb3C5Df61E9b321F5eAeB7De4DC7e024ca",
+        contractAddress: "0x953513d3AD75027120493679eC6dD024bb98ee10",
     }
 }
 
